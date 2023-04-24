3709756a5b6e961c8844dd71a68cf510de76573f
Logan Hankla <chankla@smith.edu>
2023-04-24 14:53:31 -0400

    nurse

---
+++ People/nurse.java
@@ -0,0 +1,9 @@
+public class nurse extends person {
+    
+    public nurse(String name){
+        super(name);
+    }
+
+    public getTreatOptions();
+        System.out.println("* disinfect" + "\n * bandaid");
+}

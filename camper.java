b28307a41428bd72f7d501585ed882f81f410c3f
Logan Hankla <chankla@smith.edu>
2023-04-26 13:11:03 -0400

    small changes to camper, made main to test

--- camper.java
+++ camper.java
@@ -14,11 +14,17 @@ public class camper extends person {
     }
 
     public void complain(){
-        String[] initialComplaint = {"My ankle hurts."};
+        String[] initialComplaint = {"My ankle hurts."}; //add more and maybe more detail? 
+        // how to give responses to any follow up questions from nurse? and how to differentiate severity levels
         Random complaint = new Random();
         int index = complaint.nextInt(initialComplaint.length);
         System.out.println(initialComplaint[index]);  //maybe make a way to remove that complaint so they don't get repeats?
 
+    }
+
+    public static void main(String[] args) {
+        camper matilda = new camper("Matilda");
+        matilda.enter();
     }
     
 }
